d041f538e22a283e9d4c368972ad79d35d16e224
Анастасия Сырцева <1132246780@pfur.ru>
2024-12-06 19:02:53 +0300

    feat(main): lab-09

---
+++ labs/lab09/report/lab9-4.asm
@@ -0,0 +1,42 @@
+%include 'in_out.asm'
+
+SECTION .data
+msg DB 'Результат: ',0
+SECTION .text
+GLOBAL _start
+_start:
+pop ecx
+
+pop edx
+
+sub ecx,1
+
+mov esi,0
+
+next:
+cmp ecx,0
+jz _end
+
+pop eax
+call atoi
+call _calcul
+
+add esi,eax
+
+loop next
+
+_end:
+mov eax,msg
+call sprint
+mov eax,esi
+call iprintLF
+call quit
+
+_calcul:
+
+mov ebx,2
+mul ebx
+
+add eax,15
+ 
+ ret
